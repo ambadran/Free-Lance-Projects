f258bb7288b888cb78cceb5195afaea55f048a6c
Abdulrahman Badran <ambadran717@hotmail.com>
2024-11-05 20:02:51 +0200

    finished unipolar driver project

---
+++ unipolar_arduino_driver/Programming/testing/testing.ino
@@ -0,0 +1,29 @@
+#include <Stepper.h>
+
+#define MOTOR_PIN1 5
+#define MOTOR_PIN2 4
+#define MOTOR_PIN3 3
+#define MOTOR_PIN4 2
+
+// Defines the number of steps per rotation
+const int stepsPerRevolution = 2038;
+
+// Creates an instance of stepper class
+// Pins entered in sequence IN1-IN3-IN2-IN4 for proper step sequence
+Stepper myStepper = Stepper(stepsPerRevolution, MOTOR_PIN1, MOTOR_PIN2, MOTOR_PIN3, MOTOR_PIN4);
+
+void setup() {
+    // Nothing to do (Stepper Library sets pins as outputs)
+}
+
+void loop() {
+	// Rotate CW slowly at 5 RPM
+	myStepper.setSpeed(5);
+	myStepper.step(stepsPerRevolution);
+	delay(1000);
+	
+	// Rotate CCW quickly at 10 RPM
+	myStepper.setSpeed(10);
+	myStepper.step(-stepsPerRevolution);
+	delay(1000);
+}
