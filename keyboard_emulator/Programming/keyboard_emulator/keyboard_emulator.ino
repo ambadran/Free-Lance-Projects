9d1aee083bd18ee6def0a219139a70bbd1b73edb
ambadran <ambadran717@hotmail.com>
2024-12-29 16:05:39 +0200

    finished keyboard emulator project

--- keyboard_emulator/Programming/keyboard_emulator/keyboard_emulator.ino
+++ keyboard_emulator/Programming/keyboard_emulator/keyboard_emulator.ino
@@ -1,16 +1,40 @@
 #include <Keyboard.h>
 
-const int buttonPin = 9; // Button connected to digital pin 2
+const int buttonPin0 = 21;
+const int buttonPin1 = 20;
+const int buttonPin2 = 2;
+const int buttonPin3 = 3;
 
 void setup() {
-  pinMode(buttonPin, INPUT_PULLUP); // Use internal pull-up resistor
+  pinMode(buttonPin0, INPUT_PULLUP); 
+  pinMode(buttonPin1, INPUT_PULLUP); 
+  pinMode(buttonPin2, INPUT_PULLUP); 
+  pinMode(buttonPin3, INPUT_PULLUP); 
   Keyboard.begin();
 }
 
 void loop() {
-  if (digitalRead(buttonPin) == LOW) { // Button pressed
-    Keyboard.print('B'); // Types 'A' 
-    delay(300); // Prevent spamming
+  if (digitalRead(buttonPin0) == LOW) { 
+    Keyboard.print(' ');
+    delay(300); 
+  }
+  if (digitalRead(buttonPin1) == LOW) { 
+    Keyboard.press(KEY_RETURN);
+    delay(100); 
+    Keyboard.releaseAll(); 
+    delay(300); 
+  }
+  if (digitalRead(buttonPin2) == LOW) { 
+    Keyboard.press(KEY_UP_ARROW); // Simulate Up Arrow
+    delay(100);
+    Keyboard.releaseAll();
+    delay(300); 
+  }
+  if (digitalRead(buttonPin3) == LOW) { 
+    Keyboard.press(KEY_RIGHT_ARROW); // Simulate Right Arrow
+    delay(100);
+    Keyboard.releaseAll();
+    delay(300); 
   }
 }
 
